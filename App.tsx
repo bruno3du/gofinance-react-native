476be2b3f8d31fad10b92947df24f16ed0e13b58
bruno3du <bruno.3duardo@hotmail.com>
2022-07-17 16:28:47 -0300

    added styled components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
 /** @format */
 
-import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+
 import Dashboard from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
+import {
+	useFonts,
+	Poppins_400Regular,
+	Poppins_500Medium,
+	Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
-	return <Dashboard />;
+	const [fontsLoaded] = useFonts({
+		Poppins_400Regular,
+		Poppins_500Medium,
+		Poppins_700Bold,
+	});
+
+	if(!fontsLoaded) {
+		return <AppLoading />
+	}
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Dashboard />
+		</ThemeProvider>
+	);
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,20 +1,9 @@
-/** @format */
-
-import { View, Text, StyleSheet } from 'react-native';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export default function Dashboard() {
 	return (
-		<Container style={styles.container}>
-			<Text>Dashboard dsds</Text>
+		<Container>
+			<Title>Dashboard dsd dsdss</Title>
 		</Container>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-});
